f6e15973e4e332b45f4c81d68b4f22e1b40fa221
Ryan Yogan <ryanyogan@gmail.com>
2013-10-31 01:19:17 -0500

    Added a new modal to add players

---
+++ Ratings/PlayerDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerDetailsViewController.h
+//  Ratings
+//
+//  Created by Ryan Yogan on 10/31/13.
+//  Copyright (c) 2013 Ryan Yogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerDetailsViewController : UITableViewController
+
+@end
